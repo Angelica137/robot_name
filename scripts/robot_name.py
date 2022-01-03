2cd51aafd4247f3c782a801c2ded70854b7c3bfe
Angelica <Angelica137@users.noreply.github.com>
2022-01-03 20:00:48 +0000

    no collisions, red

--- scripts/robot_name.py
+++ scripts/robot_name.py
@@ -2,8 +2,10 @@ import string
 import random
 
 
+used = []
+
+
 def generateName():
-    #names_used = set()
     characters = []
     for j in range(0, 2):
         characters.append(random.choice(string.ascii_uppercase))
@@ -11,28 +13,7 @@ def generateName():
         characters.append(str(random.randint(0, 9)))
     separator = ''
     randomName = separator.join(characters)
-    # if randomName not in names_used:
-    #    names_used.add(randomName)
-
     return randomName
-
-
-taken_names = {}
-
-
-class Robot:
-    def __init__(self):
-        self.name = generateName()
-        self.used = {}
-
-    def reset(self):
-        self.name = generateName()
-
-    def check_name(self):
-        while self.name in self.used:
-            self.name = generateName()
-        self.used.add(self.name)
-        return self.used
 
 
 def check_name(str, set_names):
@@ -42,18 +23,34 @@ def check_name(str, set_names):
     return set_names
 
 
-s = {'a', 'b', 'angelica'}
-word = "angelica"
+class Robot:
+    def __init__(self):
+        self.name = self.get_name()
 
-print(check_name(word, s))
+    def reset(self):
+        self.name = generateName()
+        self.name = self.check_name()
+        return self.name
+
+    def check_name(self):
+        while self.name in used:
+            self.name = generateName()
+        used.append(self.name)
+        return self.name
+
+    def get_name(self):
+        self.name = generateName()
+        return self.name
+
 
 r = Robot()
-name = r.name
-r.reset()
+print(r.name)
+print(r.reset())
 name2 = r.name
-print(name)
-print(r.used)
 print(name2)
 
 r2 = Robot()
 print(r2.name)
+
+
+print(used)
