564aa6bd0031d0b350ba468255aff15cac1be717
Angelica <Angelica137@users.noreply.github.com>
2022-01-03 19:12:49 +0000

    create a set to store the names

--- scripts/robot_name.py
+++ scripts/robot_name.py
@@ -2,11 +2,8 @@ import string
 import random
 
 
-"stores the names currenlty in use"
-active_robots = ()
-
-
 def generateName():
+    #names_used = set()
     characters = []
     for j in range(0, 2):
         characters.append(random.choice(string.ascii_uppercase))
@@ -14,32 +11,49 @@ def generateName():
         characters.append(str(random.randint(0, 9)))
     separator = ''
     randomName = separator.join(characters)
+    # if randomName not in names_used:
+    #    names_used.add(randomName)
+
     return randomName
+
+
+taken_names = {}
 
 
 class Robot:
     def __init__(self):
         self.name = generateName()
+        self.used = {}
 
     def reset(self):
         self.name = generateName()
 
-    def name_avail(self):
-        "checks is the name generated is available"
-        if self.name in active_robots:
+    def check_name(self):
+        while self.name in self.used:
             self.name = generateName()
-        else:
-            active_robots.add(self.name)
+        self.used.add(self.name)
+        return self.used
 
+
+def check_name(str, set_names):
+    while str in set_names:
+        str = generateName()
+    set_names.add(str)
+    return set_names
+
+
+s = {'a', 'b', 'angelica'}
+word = "angelica"
+
+print(check_name(word, s))
 
 r = Robot()
 name = r.name
 r.reset()
 name2 = r.name
 print(name)
+print(r.used)
 print(name2)
 
 r2 = Robot()
 print(r2.name)
-
-print(active_robots)
