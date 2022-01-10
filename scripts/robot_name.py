860d26048e3585f24cb6c7f3bde1c6b970f2db7a
Angelica <Angelica137@users.noreply.github.com>
2022-01-10 09:50:07 +0000

    amend method and var names

--- scripts/robot_name.py
+++ scripts/robot_name.py
@@ -5,14 +5,14 @@ import random
 used = set()
 
 
-def generateName():
+def generate_name():
     name_parts = []
     name_parts.append(
         ''.join(random.SystemRandom().choices(string.ascii_uppercase, k=2)))
     name_parts.append(
         str(random.SystemRandom().randint(100, 999)))
-    randomName = ''.join(name_parts)
-    return randomName
+    random_name = ''.join(name_parts)
+    return random_name
 
 
 class Robot:
@@ -20,17 +20,17 @@ class Robot:
         self.name = self.get_name()
 
     def reset(self):
-        self.name = generateName()
+        self.name = generate_name()
         self.name = self.check_name()
         return self.name
 
     def check_name(self):
         while self.name in used:
-            self.name = generateName()
+            self.name = generate_name()
         used.add(self.name)
         return self.name
 
     def get_name(self):
-        self.name = generateName()
+        self.name = generate_name()
         self.name = self.check_name()
         return self.name
